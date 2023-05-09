28346aea68444fddba0b280a05db4bec8cdc6d25
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-10 07:50:41 +1000

    Step 8: Add onChange event listener to other inputs

--- src/FoodOrderForm.jsx
+++ src/FoodOrderForm.jsx
@@ -13,13 +13,13 @@ function FoodOrderForm() {
             <input id="name" value={name} onChange={(e) => setName(e.target.value)}/>
 
             <label htmlFor="phone">Phone:</label>
-            <input id="phone" value={phone}/>
+            <input id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
 
             <label htmlFor="address">Address:</label>
-            <input id="address" value={address}/>
+            <input id="address" value={address} onChange={(e) => setAddress(e.target.value)}/>
 
             <label htmlFor="order">Order:</label>
-            <input id="order" value={order}/>
+            <input id="order" value={order} onChange={(e) => setOrder(e.target.value)}/>
 
             <button type="submit">Submit order</button>
         </form>
