31690293df9c98ff1a99f99e036c00df23b86de2
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-10 07:30:02 +1000

    Step 3: Add labels for name, address, phone and order

--- src/FoodOrderForm.jsx
+++ src/FoodOrderForm.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 function FoodOrderForm() {
     return (
         <form>
+            <label htmlFor="name">Name:</label>
             <input id="name" />
+
+            <label htmlFor="phone">Phone:</label>
             <input id="phone" />
+
+            <label htmlFor="address">Address:</label>
             <input id="address" />
+
+            <label htmlFor="order">Order:</label>
             <input id="order" />
         </form>
     )
