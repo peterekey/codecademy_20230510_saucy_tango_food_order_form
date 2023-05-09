450c3b421c70ba6fe109f6b61368d1273544a6f4
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-10 07:17:25 +1000

    Step 2: Add input fields for name, address, phone and order

--- src/FoodOrderForm.jsx
+++ src/FoodOrderForm.jsx
@@ -3,7 +3,10 @@ import { useState } from "react";
 function FoodOrderForm() {
     return (
         <form>
-            
+            <input id="name" />
+            <input id="phone" />
+            <input id="address" />
+            <input id="order" />
         </form>
     )
 }
