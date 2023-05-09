3a61b669c8a4c56d8c3e3045a1d6278b558c61e1
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-10 07:36:14 +1000

    Step 5: Define state variable for each piece of info

--- src/FoodOrderForm.jsx
+++ src/FoodOrderForm.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 function FoodOrderForm() {
+
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [address, setAddress] = useState('')
+    const [order, setOrder] = useState('')
+
     return (
         <form>
             <label htmlFor="name">Name:</label>
