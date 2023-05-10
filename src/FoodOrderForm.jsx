e60d0a4e1450f44a1e03e5fb9259366300e04850
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-05-11 08:19:18 +1000

    Step 12: Add name and type attribute to input elements

--- src/FoodOrderForm.jsx
+++ src/FoodOrderForm.jsx
@@ -8,7 +8,6 @@ function FoodOrderForm() {
     const [order, setOrder] = useState('')
 
     const handleSubmit = e => {
-        console.log(e)
         e.preventDefault()
         alert(`Order successful! Your order was ${order}. Please show your confirmation number for pickup.`)
     }
@@ -16,16 +15,16 @@ function FoodOrderForm() {
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="name">Name:</label>
-            <input id="name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <input id="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
 
             <label htmlFor="phone">Phone:</label>
-            <input id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+            <input id="phone" name="phone" type="number" value={phone} onChange={(e) => setPhone(e.target.value)}/>
 
             <label htmlFor="address">Address:</label>
-            <input id="address" value={address} onChange={(e) => setAddress(e.target.value)}/>
+            <input id="address" name="address" type="text" value={address} onChange={(e) => setAddress(e.target.value)}/>
 
             <label htmlFor="order">Order:</label>
-            <input id="order" value={order} onChange={(e) => setOrder(e.target.value)}/>
+            <input id="order" name="order" type="text" value={order} onChange={(e) => setOrder(e.target.value)}/>
 
             <button type="submit">Submit order</button>
         </form>
